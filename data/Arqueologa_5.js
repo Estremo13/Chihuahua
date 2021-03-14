29e3f9a26e6af5dcb3f9a06ff29e2260eaaa9fc7
Estremo13 <63829314+Estremo13@users.noreply.github.com>
2021-03-14 11:13:11 -0600

    Add files via upload

---
+++ data/Arqueologa_5.js
@@ -0,0 +1,10 @@
+var json_Arqueologa_5 = {
+"type": "FeatureCollection",
+"name": "Arqueologa_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CLAVE": "2", "LUGAR": "Paquimé", "DISTANCIA": "4 horas", "POBLACIONE": "Nuevo Casas Grandes", "ESTANCIA": "Hotel", "TIEMPO NEC": "1 día", "ECOSISTEMA": null, "ACTIVIDADE": "Sitios arqueológicos", "COMENTARIO": "No se sabe quienes la construyeron", "LONGITUD": -107.9479012, "LATITUD": 30.36845134, "Tipo": "Arqueología", "Path": "C:\/Documentos de Estremo\/Mapas\/Madrid\/Fotos\\02.jpg", "RelPath": ".\/Fotos\/02.jpg", "RelPath2": null }, "geometry": { "type": "Point", "coordinates": [ -107.9479012, 30.36845134 ] } },
+{ "type": "Feature", "properties": { "CLAVE": "19", "LUGAR": "Madera", "DISTANCIA": "4 horas", "POBLACIONE": null, "ESTANCIA": "¿Hotel?", "TIEMPO NEC": "2 días", "ECOSISTEMA": "Bosque", "ACTIVIDADE": "Ciudad donde fue el asalto al cuartel Madera", "COMENTARIO": "Lugar icónico para todo aquel que le interese la historia de las guerrillas en México, podemos parar camino a Cuarenta Casas", "LONGITUD": -108.16183310352044, "LATITUD": 29.17897436909572, "Tipo": "Arqueología", "Path": "C:\/Documentos de Estremo\/Mapas\/Madrid\/Fotos\\19.jpg", "RelPath": ".\/Fotos\/19.jpg", "RelPath2": null }, "geometry": { "type": "Point", "coordinates": [ -108.146437822956997, 29.196469348206286 ] } },
+{ "type": "Feature", "properties": { "CLAVE": "20", "LUGAR": "Cuarenta Casas", "DISTANCIA": "4 horas y media", "POBLACIONE": "Madera", "ESTANCIA": "En Madera", "TIEMPO NEC": "2 días", "ECOSISTEMA": "Bosque", "ACTIVIDADE": "Zona arqueológica", "COMENTARIO": null, "LONGITUD": -108.14363518965028, "LATITUD": 29.578461889401435, "Tipo": "Arqueología", "Path": "C:\/Documentos de Estremo\/Mapas\/Madrid\/Fotos\\20.jpg", "RelPath": ".\/Fotos\/20.jpg", "RelPath2": null }, "geometry": { "type": "Point", "coordinates": [ -108.168742349980562, 29.55648487349789 ] } }
+]
+}
---
+++ data/Chih_6.js
@@ -0,0 +1,8 @@
+var json_Chih_6 = {
+"type": "FeatureCollection",
+"name": "Chih_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CLAVE": "1", "LUGAR": "Chihuahua", "DISTANCIA": "No aplica", "POBLACIONE": "No aplica", "ESTANCIA": null, "TIEMPO NEC": null, "ECOSISTEMA": null, "ACTIVIDADE": null, "COMENTARIO": null, "LONGITUD": -106.0591711, "LATITUD": 28.63704074, "Tipo": "Ciudad de referencia", "Path": "C:\/Documentos de Estremo\/Mapas\/Madrid\/Fotos\\01.png", "RelPath": ".\/Fotos\/01.jpg" }, "geometry": { "type": "Point", "coordinates": [ -106.0591711, 28.63704074 ] } }
+]
+}
